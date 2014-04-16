feaa725f2d5c03c74e9ca9dcd72edbd3d9b76629
Kay <ksjun@mkhq.co.kr>
2014-04-16 14:25:39 +0900

    fixed a bug #89: `presentPopoverAsDialogAnimated:` does not show any popover.

--- demos/Demo/WYPopoverDemo/WYAllDirectionsViewController.m
+++ demos/Demo/WYPopoverDemo/WYAllDirectionsViewController.m
@@ -34,6 +34,7 @@
 @synthesize bottomLeftButton;
 @synthesize bottomButton;
 @synthesize bottomRightButton;
+@synthesize dialogButton;
 
 - (void)viewDidLoad
 {
@@ -122,11 +123,19 @@
         settingsPopoverController.popoverLayoutMargins = UIEdgeInsetsMake(10, 10, 10, 10);
         settingsPopoverController.wantsDefaultContentAppearance = NO;
         
-        [settingsPopoverController presentPopoverFromRect:btn.bounds
-                                                   inView:btn
-                                 permittedArrowDirections:WYPopoverArrowDirectionAny
-                                                 animated:YES
-                                                  options:WYPopoverAnimationOptionFadeWithScale];
+        if (sender == dialogButton)
+        {
+            [settingsPopoverController presentPopoverAsDialogAnimated:YES
+                                                              options:WYPopoverAnimationOptionFadeWithScale];
+        }
+        else
+        {
+            [settingsPopoverController presentPopoverFromRect:btn.bounds
+                                                       inView:btn
+                                     permittedArrowDirections:WYPopoverArrowDirectionAny
+                                                     animated:YES
+                                                      options:WYPopoverAnimationOptionFadeWithScale];
+        }
     }
     else
     {
